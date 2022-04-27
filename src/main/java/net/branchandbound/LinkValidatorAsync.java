a6808162f79e31c8c5dc2aa50211b5aa56f95d10
claudio Abud <clau@gmail.com>
2022-04-27 13:15:57 -0500

    implemented Async version of linkvalidator

---
+++ src/main/java/net/branchandbound/LinkValidatorAsync.java
@@ -0,0 +1,45 @@
+package net.branchandbound;
+
+import java.awt.color.ICC_Profile;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+public class LinkValidatorAsync {
+
+    private static HttpClient httpClient;
+
+    public static void main(String... args) throws IOException {
+        httpClient = HttpClient.newHttpClient();
+        var futures = Files.lines(Path.of("src/main/resources/urls.txt"))
+                .map(LinkValidatorAsync::validateLink)
+                .collect(Collectors.toList());
+        futures.stream()
+                .map(CompletableFuture::join)
+                .forEach(System.out::println);
+    }
+
+    private static CompletableFuture<String> validateLink(String link) {
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(link))
+                .GET()
+                .build();
+        HttpResponse.BodyHandler<Void> bodyHandler = HttpResponse.BodyHandlers.discarding();
+        CompletableFuture<HttpResponse<Void>> httpResponse = null;
+        httpResponse = httpClient.sendAsync(httpRequest, bodyHandler);
+        return httpResponse.thenApply(LinkValidatorAsync::responseToString).exceptionally(e -> String.format("(%s) -> (%s)", link, false));
+    }
+
+    private static String responseToString(HttpResponse<Void> response) {
+        int status = response.statusCode();
+        boolean isValid = status >= 200 && status <= 299;
+        return String.format("(%s) -> (%s) (status %s)", response.uri(), isValid, response.statusCode());
+    }
+
+}
